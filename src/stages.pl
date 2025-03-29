4fdc5c7f46459893c472f33db1a84abe9c5bcc58
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-03-29 17:50:27 -0300

    feat(stage): prepare base functions from stages

--- src/stages.pl
+++ src/stages.pl
@@ -1,1 +1,7 @@
 :- module(stages, []).
+
+stage1(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
+
+stage2(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
+
+stage3(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
