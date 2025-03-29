db6e2d61e16291efa01ca4fec39077c6871b40fd
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-03-29 19:31:09 -0300

    feat(stages): create stages validation rules + stages start logic

--- src/stages.pl
+++ src/stages.pl
@@ -1,7 +1,40 @@
 :- module(stages, []).
 
-stage1(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
+:- use_module("./src/validations.pl").
+:- use_module("./src/finishGame.pl").
+:- use_module("./src/window.pl").
+:- use_module("./src/board.pl").
 
-stage2(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
+stage1(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    validations:validateStage1(TotRounds),
+    stage2(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot), !.
 
-stage3(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot).
+
+stage2(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    validations:validateStage2(Matriz, Player),
+    stage3(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot), !.
+
+
+stage3(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    validations:validateStage3(Matriz),
+    finishGame:finishGame(Matriz, TotRounds, (Player, P1, P2), IsBot), !.
+
+
+
+
+stage1(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    window:showGameData(TotRounds, 1, Player, P1, P2),
+    board:boardGenerate((1, 1), Matriz),
+    get_single_char(Input).
+
+
+stage2(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    window:showGameData(TotRounds, 2, Player, P1, P2),
+    board:boardGenerate((1, 1), Matriz),
+    get_single_char(Input).
+
+
+stage3(Matriz, TotRounds, (Player, P1, P2), Mill, IsBot) :-
+    window:showGameData(TotRounds, 3, Player, P1, P2),
+    board:boardGenerate((1, 1), Matriz),
+    get_single_char(Input).
