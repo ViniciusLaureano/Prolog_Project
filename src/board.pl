569b4cd4a8d2346cdd16f60b5f13d58d52b1e2f2
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-03-29 19:29:51 -0300

    feat(board): create and prepare full board environment

--- src/board.pl
+++ src/board.pl
@@ -1,24 +1,34 @@
 :- module(board, []).
 :- use_module("./src/window.pl").
 
-imprimeTabuleiro((X, Y), Matriz) :-
-        window:centralizarV(18),
-        window:centralizarH(" *━━━━━━━━━━━━━━━━━*━━━━━━━━━━━━━━━━━*"),
-        window:centralizarH(" ┃                 ┃                 ┃"),
-        window:centralizarH(" ┃                 ┃                 ┃"),
-        window:centralizarH(" ┃     *━━━━━━━━━━━*━━━━━━━━━━━*     ┃"),
-        window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
-        window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
-        window:centralizarH(" ┃     ┃     *━━━━━*━━━━━*     ┃     ┃"),
-        window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
-        window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
-        window:centralizarH(" *━━━━━*━━━━━*           *━━━━━*━━━━━*"),
-        window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
-        window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
-        window:centralizarH(" ┃     ┃     *━━━━━*━━━━━*     ┃     ┃"),
-        window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
-        window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
-        window:centralizarH(" ┃     *━━━━━━━━━━━*━━━━━━━━━━━*     ┃"),
-        window:centralizarH(" ┃                 ┃                 ┃"),
-        window:centralizarH(" ┃                 ┃                 ┃"),
-        window:centralizarH(" *━━━━━━━━━━━━━━━━━*━━━━━━━━━━━━━━━━━*").
+boardGenerate((X, Y), Matriz) :-
+    boardBody,
+    findPieces(Matriz, 1, 1),
+    drawPointer(X, Y).
+
+findPieces([], _, _).
+findPieces([H|T], I, J).
+
+drawPointer(X, Y).
+
+boardBody() :-
+    window:centralizarV(20),
+    window:centralizarH(" *━━━━━━━━━━━━━━━━━*━━━━━━━━━━━━━━━━━*"),
+    window:centralizarH(" ┃                 ┃                 ┃"),
+    window:centralizarH(" ┃                 ┃                 ┃"),
+    window:centralizarH(" ┃     *━━━━━━━━━━━*━━━━━━━━━━━*     ┃"),
+    window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
+    window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
+    window:centralizarH(" ┃     ┃     *━━━━━*━━━━━*     ┃     ┃"),
+    window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
+    window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
+    window:centralizarH(" *━━━━━*━━━━━*           *━━━━━*━━━━━*"),
+    window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
+    window:centralizarH(" ┃     ┃     ┃           ┃     ┃     ┃"),
+    window:centralizarH(" ┃     ┃     *━━━━━*━━━━━*     ┃     ┃"),
+    window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
+    window:centralizarH(" ┃     ┃           ┃           ┃     ┃"),
+    window:centralizarH(" ┃     *━━━━━━━━━━━*━━━━━━━━━━━*     ┃"),
+    window:centralizarH(" ┃                 ┃                 ┃"),
+    window:centralizarH(" ┃                 ┃                 ┃"),
+    window:centralizarH(" *━━━━━━━━━━━━━━━━━*━━━━━━━━━━━━━━━━━*").
