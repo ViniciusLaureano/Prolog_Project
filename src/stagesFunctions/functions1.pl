215b813dab8abf836ea91f885464533e9826474d
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-04-01 22:24:59 -0300

    fix(moviment): fix board limit and valids positions

--- src/stagesFunctions/functions1.pl
+++ src/stagesFunctions/functions1.pl
@@ -1,5 +1,8 @@
 :- module(functions1, []).
 
+:- use_module("./src/board.pl").
+:- use_module("./src/window.pl").
+
 movimento(w, (-1, 0)). % Para cima
 movimento(s, (1, 0)).  % Para baixo
 movimento(a, (0, -1)). % Para a esquerda
@@ -22,8 +25,6 @@ mover_ate_proximo(Matriz, X, Y, DX, DY, NX, NY) :-
     processa_elemento(Elem, Matriz, NX1, NY1, X, Y, DX, DY, NX, NY),
     !.
 
-mover_ate_proximo(_, X, Y, _, _, X, Y).
-
 processa_elemento((-1, -1), _, _, _, X, Y, _, _, X, Y) :- !.
 
 processa_elemento((1, 0), _, NX1, NY1, _, _, _, _, NX1, NY1) :- !.
@@ -31,16 +32,21 @@ processa_elemento((1, 0), _, NX1, NY1, _, _, _, _, NX1, NY1) :- !.
 processa_elemento(_, Matriz, NX1, NY1, X, Y, DX, DY, NX, NY) :- mover_ate_proximo(Matriz, NX1, NY1, DX, DY, NX, NY).
 
 
-move_cursor(Matriz, X, Y) :-
+move_cursor(Matriz, X, Y, (TotRounds, StageNum, Player, P1, P2)) :-
     get_single_char(Input),
     char_code(Char, Input),
-    processa_entrada(Char, Matriz, X, Y).
+    processa_entrada(Char, Matriz, X, Y, NX, NY),
+    processa_jogada(Matriz, NX, NY, (TotRounds, StageNum, Player, P1, P2)).
 
-processa_entrada(Input, Matriz, X, Y) :-
+processa_entrada(Input, Matriz, X, Y, NX, NY) :-
     movimento(Input, (DX, DY)),
     mover_ate_proximo(Matriz, X, Y, DX, DY, NX, NY),
-    move_cursor(Matriz, NX, NY),
     !.
 
-processa_entrada(_, Matriz, X, Y) :-
-    move_cursor(Matriz, X, Y).
+processa_entrada(_, _, X, Y, X, Y).
+
+processa_jogada(Matriz, X, Y, (TotRounds, StageNum, Player, P1, P2)) :-
+    window:showGameData(TotRounds, StageNum, Player, P1, P2),
+    board:boardGenerate((X, Y), Matriz),
+    format("Cursor na posição (~w, ~w). Use W, A, S, D para mover:\n", [X, Y]),
+    move_cursor(Matriz, X, Y, (TotRounds, StageNum, Player, P1, P2)).
