b20553542f3860cb36cf0c21a490c59f105ab1b5
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-04-03 14:34:55 -0300

    feat(board): implement draw pieces

--- src/board.pl
+++ src/board.pl
@@ -3,14 +3,26 @@
 
 boardGenerate((X, Y), Matriz) :-
     boardBody,
-    findPieces(Matriz, 1, 1),
+    findPieces(Matriz),
     drawPointer(X, Y).
 
-findPieces([], _, _).
-findPieces([H|T], I, J).
+findPieces(Matriz) :-
+    findCellsWithValue(Matriz, 1, Result1),
+    findCellsWithValue(Matriz, 2, Result2),
+    iterateDrawPiece(Result1, 1),
+    iterateDrawPiece(Result2, 2).
+    
+iterateDrawPiece([], _).
+iterateDrawPiece([(X, Y)|T], Player) :-
+    window:drawPiece(X, Y, Player),
+    iterateDrawPiece(T, Player).
 
-drawPointer(Y, X) :-
-    window:drawPiece(Y, X, 'X').
+
+drawPointer(X, Y) :-
+    window:drawPiece(X, Y, 'X').
+
+findCellsWithValue(Matriz, X, Result) :-
+    findall((RowAjust, ColAjust), (nth1(Row, Matriz, Linha), nth1(Col, Linha, (1, X)), RowAjust is Row - 1, ColAjust is Col - 1), Result).
 
 boardBody() :-
     window:centralizarV(20),
