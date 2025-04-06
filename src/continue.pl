8ba3cc155f858d2b19d83343baa970af087f09eb
XandeCampos <alexandretcampos11@gmail.com>
2025-04-06 01:51:30 -0300

    First saving shenanigans commit

--- src/continue.pl
+++ src/continue.pl
@@ -1,1 +1,31 @@
 :- module(continue, []).
+:- use_module("./src/stages.pl").
+
+saveInterruptedGame(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot) :-
+    buildState(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot, State),
+    open("./src/json/saveGame.json", write, SaveGame),
+    json_write_dict(SaveGame, State, [width(0)]),
+    close(SaveGame).
+
+continueGame() :- 
+    open("./src/json/saveGame.json", read, SaveGame),
+    json_read_dict(SaveGame, GameState),
+    close(SaveGame),
+
+    get_dict(stage, GameState, Stage),
+    StoppedStage = Stage.stage,
+    stageCalling(Stage, StoppedStage).
+
+buildState(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot, State) :-
+    State = state{
+        matrix: Matrix,
+        totRounds: TotRounds,
+        players: (Player, P1, P2),
+        stage: Stage,
+        mill: Mill,
+        isBot: IsBot
+    }.
+
+stageCalling(Stage, "stage1") :- stage1(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
+stageCalling(Stage, "stage2") :- stage2(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
+stageCalling(Stage, "stage3") :- stage3(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
--- src/finishGame.pl
+++ src/finishGame.pl
@@ -1,3 +1,30 @@
 :- module(finishGame, []).
 
-finishGame(Matriz, TotRounds, (Player, P1, P2), IsBot).
+finishGame(Matrix, TotRounds, (Player, P1, P2), IsBot) :- 
+    buildHistoryState(Matrix, TotRounds, (Player, P1, P2), IsBot, HistoryState),
+    read_history("./json/saveHistory.json", CurrentHistory),
+    append_history(HistoryState, CurrentHistory).
+
+read_history(File, History) :-
+    exists_file(File),
+    open(File, read, Stream),
+    json_read_dict(Stream, History),
+    close(Stream), !.
+
+read_history(_, []).
+
+append_history(NewState, File) :-
+    read_history(File, History),
+    append(History, [NewState], UpdatedHistory),
+    open(File, write, Stream),
+    json_write_dict(Stream, UpdatedHistory, [width(0)]),
+    close(Stream).
+
+buildHistoryState(Matrix, TotRounds, (Player, P1, P2), IsBot, State) :-
+    State = state{
+        matrix: Matrix,
+        totRounds: TotRounds,
+        players: (Player, P1, P2),
+        isBot: IsBot
+    }.
+
