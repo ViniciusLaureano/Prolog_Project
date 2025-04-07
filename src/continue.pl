e873bd493d7153d6b616e7f91ebcff7fb64038dd
XandeCampos <alexandretcampos11@gmail.com>
2025-04-07 20:13:21 -0300

    Persistency kinda done

--- src/continue.pl
+++ src/continue.pl
@@ -1,4 +1,5 @@
 :- module(continue, []).
+:- use_module(library(http/json)).
 :- use_module("./src/stages.pl").
 
 saveInterruptedGame(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot) :-
@@ -8,6 +9,7 @@ saveInterruptedGame(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot) :-
     close(SaveGame).
 
 continueGame() :- 
+    exists_file("./src/json/saveGame.json"),
     open("./src/json/saveGame.json", read, SaveGame),
     json_read_dict(SaveGame, GameState),
     close(SaveGame),
@@ -17,15 +19,15 @@ continueGame() :-
     stageCalling(Stage, StoppedStage).
 
 buildState(Matrix, TotRounds, (Player, P1, P2), Stage, Mill, IsBot, State) :-
-    State = state{
+    State = _{
         matrix: Matrix,
         totRounds: TotRounds,
-        players: (Player, P1, P2),
+        players: [Player, P1, P2],
         stage: Stage,
         mill: Mill,
         isBot: IsBot
     }.
 
-stageCalling(Stage, "stage1") :- stage1(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
-stageCalling(Stage, "stage2") :- stage2(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
-stageCalling(Stage, "stage3") :- stage3(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
+stageCalling(Stage, stage1) :- stage1(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
+stageCalling(Stage, stage2) :- stage2(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
+stageCalling(Stage, stage3) :- stage3(Stage.matrix, Stage.totRounds, Stage.players, Stage.mill, Stage.isBot).
--- src/history.pl
+++ src/history.pl
@@ -1,12 +1,13 @@
 :- module(history, []).
+:- use_module(library(http/json)).
 
+
+read_history(File, []) :- exists_file(File), !.
 read_history(File, History) :-
     exists_file(File),
     open(File, read, Stream),
     json_read_dict(Stream, History),
     close(Stream), !.
-
-read_history(_, []).
 
 append_history(NewState, File) :-
     read_history(File, History),
@@ -16,15 +17,15 @@ append_history(NewState, File) :-
     close(Stream).
 
 saveFinalGame(Matrix, TotRounds, Winner, (P1, P2), IsBot) :-
-    buildState(Matrix, TotRounds, Winner, (P1, P2), Stage, Mill, IsBot, State),
+    buildFinalState(Matrix, TotRounds, Winner, (P1, P2), Stage, Mill, IsBot, State),
     append_history(State, "./src/json/saveHistory.json").
 
 buildFinalState(Matrix, TotRounds, Winner, (P1, P2), IsBot, State) :-
-    State = state{
+    State = _{
         matrix: Matrix,
         totRounds: TotRounds,
-        winner: Winner
-        players: (P1, P2),
+        winner: Winner,
+        players: [P1, P2],
         isBot: IsBot
     }.
 
