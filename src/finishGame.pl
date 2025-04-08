87e1ae69ad41269c80bcef81a3ce2032d1864c60
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-04-08 17:10:00 -0300

    module direct

--- src/finishGame.pl
+++ src/finishGame.pl
@@ -2,6 +2,6 @@
 :- use_module("./src/history.pl").
 
 saveGameToHistory(Matrix, TotRounds, (Player, P1, P2), IsBot) :- 
-    saveFinalGame(Matrix, TotRounds, Player, (P1, P2), IsBot).
+    history:saveFinalGame(Matrix, TotRounds, Player, (P1, P2), IsBot).
 
 
