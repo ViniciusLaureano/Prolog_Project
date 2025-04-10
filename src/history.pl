bfb7d03b7c2fc6d1c99e44e281ffab665c8dc8d8
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-04-09 22:09:12 -0300

    feat(message): win message

--- src/history.pl
+++ src/history.pl
@@ -1,6 +1,7 @@
 :- module(history, []).
 :- use_module(library(http/json)).
 :- use_module("./src/continue.pl").
+:- use_module("./src/window.pl").
 
 
 read_history(File, []) :- exists_file(File), !.
@@ -19,6 +20,13 @@ append_history(NewState, File) :-
 
 saveFinalGame(Matrix, TotRounds, Winner, (P1, P2), IsBot) :-
     continue:matrix_to_json(Matrix, FormatedMatrix),
+    window:limparTela,
+    window:centralizarV(2),
+    ( Winner = 1 -> Vencedor = P1 ; Vencedor = P2 ),
+    format(atom(Mensagem), 'Parabéns ~w por vencer!!!', [Vencedor]),
+    window:centralizarH(Mensagem),
+    window:centralizarH('Aperte qualquer tecla para continuar...'),
+    get_single_char(_),
     buildFinalState(FormatedMatrix, TotRounds, Winner, (P1, P2), IsBot, State),
     append_history(State, "./src/json/saveHistory.json").
 
